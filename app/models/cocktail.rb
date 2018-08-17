85a655d343ca63fee8e3ad5d55af6db1755636ad
sophie liu <sophieliu@hotmail.fr>
2018-08-17 15:15:06 +0200

    corrected some bug

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,5 @@ class Cocktail < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  mount uploader :photo, PhotoUploader
+  mount_uploader :photo, PhotoUploader
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,5 +2,10 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+
   # Remove everything else
 end
